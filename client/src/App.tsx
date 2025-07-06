683fac72adf1847672680b1633bd29ef2d2b2cd6
mnahsanofficial <mnahsanofficial@gmail.com>
2025-07-06 22:11:57 +0600

    Added react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,39 @@
 import { Box, Typography } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import NotFound from "./pages/NotFound";
+import Dashboard from "./pages/Dashboard";
+import AppLayout from "./components/layout/AppLayout";
 
 function App() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <Box>
-            <Typography variant="h1">Bookmarks</Typography>
-          </Box>
-        }
-      />
-      <Route
-        path="bookmarks"
-        element={
-          <Box>
-            <Typography variant="h1">Bookmarks</Typography>
-          </Box>
-        }
-      />
-      <Route
-        path="archive"
-        element={
-          <Box>
-            <Typography variant="h1">Archive</Typography>
-          </Box>
-        }
-      />
-      <Route
-        path="settings"
-        element={
-          <Box>
-            <Typography variant="h1">Settings</Typography>
-          </Box>
-        }
-      />
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route
+          path="bookmarks"
+          element={
+            <Box>
+              <Typography variant="h1">Bookmarks</Typography>
+            </Box>
+          }
+        />
+        <Route
+          path="archive"
+          element={
+            <Box>
+              <Typography variant="h1">Archive</Typography>
+            </Box>
+          }
+        />
+        <Route
+          path="settings"
+          element={
+            <Box>
+              <Typography variant="h1">Settings</Typography>
+            </Box>
+          }
+        />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
