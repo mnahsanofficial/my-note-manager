d8bd0b621f7cab290c09c6bd1de4f1867a56427d
mnahsanofficial <mnahsanofficial@gmail.com>
2025-07-06 22:29:21 +0600

    Added drawer

--- client/src/components/layout/AppLayout.tsx
+++ client/src/components/layout/AppLayout.tsx
@@ -1,10 +1,113 @@
-import { Box } from "@mui/material";
+import { AppBar, Avatar, Box, Drawer, IconButton, Toolbar, Tooltip, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import { Outlet } from "react-router-dom";
+const DRAWER_WIDTH = 280;
+
+import React from "react";
 
 export default function AppLayout() {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
   return (
-    <Box>
-      <Outlet />
+    <Box sx={{ display: "flex", minHeight: "100vh" }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { md: `calc(100% - ${DRAWER_WIDTH}px)` },
+          ml: { md: `${DRAWER_WIDTH}px` },
+          bgcolor: "background.paper",
+          color: "text.primary",
+          boxShadow: 1,
+          borderBottom: "1px solid",
+          borderBottomColor: "divider",
+        }}
+      >
+        <Toolbar>
+          <IconButton sx={{ mr: 2, display: { md: "none" } }}>
+            <MenuIcon />
+          </IconButton>
+
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            {/* Page title will be set by individual pages */}
+          </Typography>
+
+          <Tooltip title="User Profile">
+            <IconButton sx={{ p: 0 }}>
+              <Avatar alt="Nazmul Ahsan" sx={{ width: 32, height: 32 }}>
+                NA
+              </Avatar>
+            </IconButton>
+          </Tooltip>
+        </Toolbar>
+      </AppBar>
+
+      {/* Navigation Drawer */}
+      <Box
+        component="nav"
+        sx={{ width: { md: DRAWER_WIDTH }, flexShrink: { md: 0 } }}
+      >
+        {/* Mobile drawer */}
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{ keepMounted: true }}
+          sx={{
+            display: { xs: "block", md: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: DRAWER_WIDTH,
+            },
+          }}
+        ></Drawer>
+
+        {/* Desktop drawer */}
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", md: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: DRAWER_WIDTH,
+              borderRight: "1px solid",
+              borderRightColor: "divider",
+            },
+          }}
+          open
+        >
+            <Toolbar /> {/* Spacer for fixed app bar */}
+            <Box sx={{ overflow: "auto" }}>
+                {/* Navigation links will go here */}
+                <Typography variant="h6" sx={{ p: 2 }}>
+                Navigation
+                </Typography>
+                {/* Example links */}
+                <Box sx={{ p: 2 }}>
+                <Typography variant="body1">Dashboard</Typography>
+                <Typography variant="body1">Bookmarks</Typography>
+                <Typography variant="body1">Archive</Typography>
+                <Typography variant="body1">Settings</Typography>
+                </Box>
+            </Box>
+        </Drawer>
+      </Box>
+
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          width: { md: `calc(100% - ${DRAWER_WIDTH}px)` },
+          bgcolor: "background.default",
+          minHeight: "100vh",
+        }}
+      >
+        <Toolbar /> {/* Spacer for fixed app bar */}
+        <Outlet />
+      </Box>
     </Box>
   );
 }
